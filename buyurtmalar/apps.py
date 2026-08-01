d00864228d1c11a274765a732f6f20c064353bc5
TNnodirbek <168888828+TNnodirbek@users.noreply.github.com>
2026-08-01 13:54:12 +0500

    App'lar va sozlamalar yangilandi

---
+++ buyurtmalar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BuyurtmalarConfig(AppConfig):
+    name = 'buyurtmalar'
---
+++ dokonlar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DokonlarConfig(AppConfig):
+    name = 'dokonlar'
