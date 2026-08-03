25906984c69cffa4d83dfa5c2f419f0ddf36b30f
TNnodirbek <168888828+TNnodirbek@users.noreply.github.com>
2026-08-03 11:38:06 +0500

    Fura qo'shish formasi to'liq dizayn bilan yozildi

--- ombor/forms.py
+++ ombor/forms.py
@@ -7,3 +7,8 @@ class FuraForm(forms.ModelForm):
     class Meta:
         model = Fura
         fields = ['sana', 'suv_narxi', 'dastavka_narxi']
+        widgets = {
+            'sana': forms.DateInput(attrs={'type': 'date', 'class': 'stil-input'}),
+            'suv_narxi': forms.NumberInput(attrs={'class': 'stil-input', 'step': '0.01', 'min': '0'}),
+            'dastavka_narxi': forms.NumberInput(attrs={'class': 'stil-input', 'step': '0.01', 'min': '0'}),
+        }
